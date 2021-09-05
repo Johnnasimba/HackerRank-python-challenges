250bedd122e651ceb6248a2e7aef96c091ceda15
JOHN NASIMBA <nasimba4john@gmail.com>
2021-09-05 19:38:12 +0200

    set intersection

---
+++ set intersection/index.py
@@ -0,0 +1,7 @@
+n_english = int(input())
+d_english = set(list(map(int, input().split())))
+
+n_french = int(input())
+d_french = set(list(map(int, input().split())))
+
+print(len(d_english & d_french))
